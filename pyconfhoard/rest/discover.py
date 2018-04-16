87306b78b3526dc7874696144d444a2d478039eb
Adam Allen <allena29@users.noreply.github.com>
2018-04-16 20:12:59 +0100

    Cleanup for REST server

--- pyconfhoard/rest/discover.py
+++ pyconfhoard/rest/discover.py
@@ -8,15 +8,15 @@ class Resource(object):
         req.get_header('TOKEN')
 
         datastores ={}
-        base = '../datastore/registered'
+        base = 'datastore/registered'
         for item in os.listdir(base):
             if item[-4:] == '.pch':
-                o = open('../datastore/registered/%s' % (item))
+                o = open('datastore/registered/%s' % (item))
                 metadata = json.loads(o.read())
                 o.close()
                 datastores[item[:-4]] = metadata
 
-        yin = open('../yang/schema.json')    
+        yin = open('yang/schema.json')    
         schema = json.loads(yin.read())
         yin.close()
         resp.body = json.dumps({'datastores': datastores, 'schema': schema})
--- pyconfhoard/rest/server.py
+++ pyconfhoard/rest/server.py
@@ -1,10 +1,13 @@
 import falcon
+import sys
+import os
+
+sys.path.append('pyconfhoard')
+sys.path.append('pyconfhoard/rest')
 
 import datastore
 import discover
-
 api = application = falcon.API()
-
 
 
 datastore_handler = datastore.Resource()
