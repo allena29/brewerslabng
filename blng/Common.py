e70c4e2e9066428e91d1d05806fc6b367801e788
Adam Allen <allena29@users.noreply.github.com>
2018-09-18 21:12:18 +0000

    Add basic HTTP class
    There is a constraint that we can get stuck in TIME_WAIT (http_server.server_close() isnt' enough) but closing python clears up the sockets immediately.

--- blng/Common.py
+++ blng/Common.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 
-import blng.Multicast as Multicast
+import time
 import blng.LogHandler as LogHandler
+import blng.Multicast as Multicast
+import blng.Http as Http
 
 
 class Common:
 
     MCAST_PORT = 4999
+    HTTP_PORT = 4998
+    HTTP_LISTEN_PORT = 4997
+    LOG_COMPONENT = 'DummyComponent'
 
     def __init__(self):
-        self.__mcasthandler = None
+        self.__mcastreceiver = None
+        self.__mcastsender = None
+        self.__httpreceiver = None
 
         if not hasattr(self, 'ID'):
             # TODO: pick up ID from os.environ
@@ -21,3 +28,31 @@ class Common:
         if hasattr(self, 'init') and callable(getattr(self, 'init')):
             sub_init = getattr(self, 'init')
             sub_init()
+
+    def subscribe(self):
+        """
+        Subscribe to receive data, each time a valid 1200byte payload is received we will
+        invoke the callback passing in a JSON decoded object.
+        """
+        if hasattr(self, '__mcastsocket'):
+            # TODO: do something to unsubscribe irst
+            pass
+
+        self.__mcastreceiver = Multicast.Multicast()
+        try:
+            self.__mcastsocket = self.__mcastreceiver.open_socket(self.multicast_receive_callback, self.MCAST_PORT)
+        except KeyboardInterrupt:
+            pass
+
+    def multicast_receive_callback(self, xxx):
+        print('override multicast_receive_callback - not re-implemented in child class')
+
+    def http_listen(self):
+        self.__httpreceiver = Http.Http()
+        try:
+            self.__httpreceiver.serve()
+            while 1:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+        self.__httpreceiver.end_serve()
---
+++ blng/Http.py
@@ -0,0 +1,59 @@
+import http.server
+import socketserver
+import threading
+import blng.LogHandler as LogHandler
+
+
+class HttpHandler(http.server.SimpleHTTPRequestHandler):
+    pass
+
+
+class Http:
+
+    HTTP_PORT = 4998
+    HTTP_ADDRESS = ""
+
+    def __init__(self, http_port=0):
+        self.groot = LogHandler.LogHandler('Http')
+        if http_port:
+            self.HTTP_PORT = http_port
+
+    def http_callback_post(self, path, post):
+        print('http_callback_post not defined in child class')
+
+    def http_callback_get(self, path):
+        print('http_callback_get not defined in child class')
+
+    def serve(self):
+        self.http_server = socketserver.TCPServer((self.HTTP_ADDRESS, self.HTTP_PORT), HttpHandler)
+        self.http_server_thread = threading.Thread(target=self.http_server.serve_forever)
+        self.http_server_thread.daemon = True
+        self.http_server_thread.start()
+
+        self.http_server.RequestHandlerClass.callback_POST = self.http_callback_post
+        self.http_server.RequestHandlerClass.callback_GET = self.http_callback_get
+
+        def do_POST(self):
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+
+            content_len = int(self.headers.get('content-length', 0))
+            post_body = self.rfile.read(content_len)
+            print('post %s %s' % (content_len, post_body))
+            self.callback_POST(self.path, post_body)
+        #    self.http_post_callback(self.path, post_body)
+
+        def do_GET(self):
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+
+            self.callback_GET(sel.path)
+
+        self.http_server.RequestHandlerClass.do_POST = do_POST
+        self.http_server.RequestHandlerClass.do_GET = do_GET
+
+    def end_serve(self):
+        self.http_server.shutdown()
+        self.http_server.server_close()
