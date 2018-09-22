de8ccb3510ccc8a35bc4e81c9135b77b7e0ea230
Adam Allen <allena29@users.noreply.github.com>
2018-09-22 20:58:33 +0000

    Implement Generic TCP server to receive and re-broadcast iSpindel data

--- broadcastISpindelGravity.py
+++ broadcastISpindelGravity.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import json
+import time
 import blng.Common
 
 
@@ -16,25 +18,53 @@ class BroadcastISpindelGravity(blng.Common.Common):
 
 
     MCAST_PORT = 5092
-    HTTP_PORT = 9501
+    TCP_PORT = 9501
     LOG_COMPONENT = 'BroadcastISpindel'
+
+    FORMULA_1 = 0.008626076
+    FORMULA_2 = 0.439419453
+    FORMULA_3 = 4.677151587
 
     def init(self):
         self.log.debug('Intialised to broadcast iSpindel Stats')
 
+    def convertAngle(self, angle):
+        """ Return the angle converted into Plato"""
+        return self.FORMULA_1 * angle **2 - self.FORMULA_2 * angle + self.FORMULA_3
 
-    def http_callback_post(self, path, json):
+    def convertPlatoToSG(self, plato):
+        """ Return specific gravity (i.e. 1.xxxx)"""
+        return 1 + (plato / (258.6 - ( (plato/258.2) *227.1) ) )
+
+    def tcp_callback(self, data):
         """
         This method is invoked as a callback from the inherited subscriber class which
+        will be invoked everytime something connects to send data on the TCP_PORT
         will invoked every time there is 1200 bytes of temperature.
 
-        TODO: we need to convert probeId mappings into Netconf datastore - not read from config file.
-        TODO: we need to move target temperatures into Netconf datastore - not broadcast from governor
+        The data we receive will is JSON-ish 
+        {'b'"name":"iSpindel000","ID":14039613,"token":"xxx","angle":42.89461,
+         "temperature":16.75,"battery":4.176225,"gravity":3.501669,"interval":600}
         """
-        print ('child do post %s' % (json))
+        try:
+            clean_data = str(data).replace("'b'\"", "\"")[2:-5]
+            json_data = json.loads(clean_data)
+            msg_dict = {}
+            msg_dict['iSpindelTemp'] = float(json_data['temperature'])
+            msg_dict['iSpindelTilt'] = float(json_data['angle'])
+            msg_dict['iSpindelBattery'] = float(json_data['battery'])
+            msg_dict['iSpindelReportedGravityPlato'] = float(json_data['gravity'])
+            msg_dict['iSpindelCalculatedGravityPlato'] = self.convertAngle(float(json_data['angle']))
+            msg_dict['iSpindelCalculatedGravitySpecific'] = self.convertPlatoToSG(self.convertAngle(float(json_data['angle'])))
+            msg_dict['timestamp'] = time.time()
+
+            self.broadcast(msg_dict)
+            self.log.info('Gravity %s' % (msg_dict['iSpindelCalculatedGravitySpecific']))
+        except Exception as e:
+            self.log.error('Unable to decode and broadcast data: %s\n %s' % (str(data), str(e)))
 
 
 if __name__ == '__main__':
     broadcast = BroadcastISpindelGravity()
-    broadcast.http_listen()
+    broadcast.tcp_listen()
 
