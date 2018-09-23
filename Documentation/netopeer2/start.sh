e01e0d6c837b4ffc2b462cb1f544e4bb3ab03037
Adam Allen <allena29@users.noreply.github.com>
2018-09-23 23:41:28 +0100

    Add startup script for netopeer docker image.
    This will also give persistence to data in <repo>/persist/sysrepo which
    will never be sent to github.

--- Documentation/netopeer2/start.sh
+++ Documentation/netopeer2/start.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+cd /
+tar cvpf /tmp/sysrepo.tar sysrepo
+rm -fr /sysrepo
+mkdir -p /brewerslabng/persist/sysrepo
+ln -s /brewerslabng/persist/sysrepo /sysrepo
+tar xvf /tmp/sysrepo.tar
+rm -fr /tmp/sysrepo.tar
+
+sysrepod
+netopeer2-server
+
+while true; do
+sleep 10
+done
+
+
