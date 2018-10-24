844eebdcfd21fff1b4ca79d613bf8d463cbf9f1f
Adam Allen <allena29@users.noreply.github.com>
2018-10-25 00:10:18 +0100

    Groupings no longer use the fake cruxuses nodes

--- blng/Munger.py
+++ blng/Munger.py
@@ -22,8 +22,8 @@ class Munger:
     NAMESPACES = {"yin": "urn:ietf:params:xml:ns:yang:yin:1"}
 
     def munge(self, module):
-        self.outstanding_types = {}
-        self.outstanding_uses = {}
+        self.typedef_map = {}
+        self.grouping_map = {}
         self.trail = []
         format = "%(asctime)-15s - %(name)-20s %(levelname)-12s  %(message)s"
         logging.basicConfig(level=logging.DEBUG, format=format)
@@ -35,12 +35,8 @@ class Munger:
         # The main module we care about
         self.xmldoc = etree.parse(".cache/%s.yin" % (module))
         xmldoc = etree.parse(".cache/%s.yin" % (module)).getroot()
-        print(xmldoc)
         self.pass1_parse_and_recurse('integrationtest', xmldoc)
-
         self.pass2_stitch_and_recurse(xmldoc)
-        print(self.outstanding_types)
-        print(self.outstanding_uses)
 
         xml_string = str(etree.tostring(xmldoc, pretty_print=True))
         o = open('z.xml', 'w')
@@ -50,13 +46,12 @@ class Munger:
     def pass1_parse_and_recurse(self, module_name, xmldoc):
         """The first pass parsing builds an index of groups and typedefs"""
         for child in xmldoc.getchildren():
-            #            print(child.tag, child.text, child.attrib.keys())
             if child.tag == "{urn:ietf:params:xml:ns:yang:yin:1}typedef":
                 self.log.debug('Adding typedef to the list %s', child.attrib['name'])
-                self.outstanding_types["%s:%s" % (module_name, child.attrib['name'])] = child
+                self.typedef_map["%s:%s" % (module_name, child.attrib['name'])] = child
             elif child.tag == "{urn:ietf:params:xml:ns:yang:yin:1}grouping":
                 self.log.debug('Adding grouping to the list %s', child.attrib['name'])
-                self.outstanding_uses["%s:%s" % (module_name, child.attrib['name'])] = child
+                self.grouping_map["%s:%s" % (module_name, child.attrib['name'])] = child
             elif child.tag == "{urn:ietf:params:xml:ns:yang:yin:1}import":
                 self.log.debug('Recursing because of import %s', child.attrib['module'])
                 child_xmldoc = etree.parse(".cache/%s.yin" % (child.attrib['module'])).getroot()
@@ -85,7 +80,6 @@ class Munger:
         children = xmldoc.getchildren()
         for child_index in range(len(children)):
             child = children[child_index]
-        # for child in xmldoc.getchildren():
             if child.tag == "{urn:ietf:params:xml:ns:yang:yin:1}import":
                 for grandchild in child.getchildren():
                     if grandchild.tag == "{urn:ietf:params:xml:ns:yang:yin:1}prefix":
@@ -93,61 +87,54 @@ class Munger:
 
             new_child = self._lookup_method(child)(child)
 
-        print(prefix_map, '<<<<prefix map')
+    def handle_container(self, child, grandchild_id=-1):
+        grandchildren = child.getchildren()
+        for grandchild_id in range(len(grandchildren)):
+            grandchild = grandchildren[grandchild_id]
+            self._lookup_method(grandchild)(grandchild, grandchild_id)
 
-    def handle_container(self, child):
-        print("CPONMTINAER", child.attrib.keys(), child.text, child.tag)
-        for grandchild in child.getchildren():
-            self._lookup_method(grandchild)(grandchild)
-
-    def handle_leaf(self, child):
+    def handle_leaf(self, child, grandchild_id=-1):
         grandchildren = child.getchildren()
 
         for grandchild_id in range(len(grandchildren)):
             grandchild = grandchildren[grandchild_id]
-            replace_grandchild = []
+            replace_grandchildren = []
             if grandchild.tag == "{urn:ietf:params:xml:ns:yang:yin:1}type":
                 type = grandchild.attrib['name']
                 if type in ('string', 'boolean'):
                     custom_type = etree.Element("cruxtype", type=type)
                     child.append(custom_type)
-                elif ':' not in type and '%s:%s' % (self.our_prefix, type) in self.outstanding_types:
-                    for stranger in self.outstanding_types['%s:%s' % (self.our_prefix, type)]:
+                elif ':' not in type and '%s:%s' % (self.our_prefix, type) in self.typedef_map:
+                    for stranger in self.typedef_map['%s:%s' % (self.our_prefix, type)]:
                         if stranger.tag == "{urn:ietf:params:xml:ns:yang:yin:1}type":
-                            replace_grandchild.append((grandchild_id, stranger, grandchild))
+                            replace_grandchildren.append((grandchild_id, stranger, grandchild))
 
-                elif ':' in type and type in self.outstanding_types:
-                    for stranger in self.outstanding_types['%s' % (type)]:
+                elif ':' in type and type in self.typedef_map:
+                    for stranger in self.typedef_map['%s' % (type)]:
                         if stranger.tag == "{urn:ietf:params:xml:ns:yang:yin:1}type":
-                            replace_grandchild.append((grandchild_id, stranger, grandchild))
+                            replace_grandchildren.append((grandchild_id, stranger, grandchild))
                 else:
-                    print(self.outstanding_types)
-                    print(self.our_prefix)
                     raise Error.BlngYangTypeNotSupported(type)
 
-        for (index, new_grandchild, old_grandchild) in replace_grandchild:
+        for (index, new_grandchild, old_grandchild) in replace_grandchildren:
             child.insert(index, new_grandchild)
             child.remove(old_grandchild)
 
-    def handle_list(self, child):
-        print(child.tag, child.text, child.attrib.keys(), "<<<<<<<<<<<<<<<< LIST")
+    def handle_list(self, child, grandchild_id=-1):
+        pass
 
-    def handle_uses(self, child):
-        for grandchild in child.getchildren():
-            print(grandchild.tag, grandchild.text, grandchild.attrib.keys(), "<<< grandchild of uses")
+    def handle_uses(self, child, grandchild_id=-1):
         if ":" not in child.attrib['name']:
             uses = "%s:%s" % (self.our_prefix, child.attrib['name'])
         else:
             uses = "%s" % (child.attrib['name'])
-        custom = etree.Element("cruxuses")
-        child.append(custom)
-        custom.append(self.outstanding_uses[uses])
-        print(child.tag, child.text, child.attrib.keys(), child.attrib['name'], uses, "<<<<<<<<<<<<<<<< USES")
+        parent = child.getparent()
+        for new_child in self.grouping_map[uses].getchildren():
+            parent.append(new_child)
+        parent.remove(child)
 
-    def handle_null(self, child=None):
-        if child:
-            print(child.tag, child.text, child.attrib.keys(), "<<<<<<<<<<<<<<<<")
-        # pass
+    def handle_null(self, child=None, grandchild_id=-1):
+        pass
 
     def _lookup_method(self, child):
         if child.tag == "{urn:ietf:params:xml:ns:yang:yin:1}leaf":
