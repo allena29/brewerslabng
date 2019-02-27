bb01f20b5d1657f24d83005ad8ea2b8f40e2a528
Adam Allen <allena29@users.noreply.github.com>
2019-02-27 01:53:22 +0000

    Before trying to return more vooodoo

--- voodoo-playing.py
+++ voodoo-playing.py
@@ -16,6 +16,12 @@ class CruxVoodoo:
                 schema = child
         return CruxVoodooNode(schema, xmldoc)
 
+
+class BadVoodoo(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
+
 class CruxVoodooNode:
 
     def __init__(self, schema, xmldoc, curpath="/"):
@@ -23,12 +29,64 @@ class CruxVoodooNode:
         self.__dict__['_curpath'] = curpath
         self.__dict__['_xmldoc'] = xmldoc
         self.__dict__['_schema'] = schema
+        self.__dict__['_type'] = 'wtf'
+
 
     def __getattr__(self, attr):
+        curpath = self.__dict__['_curpath']
         print('Get attr ', self.__dict__['_curpath'] + '/'+ attr)
+
+        xmldoc = self.__dict__['_xmldoc']
+        this_value = xmldoc.xpath(curpath + '/' + attr)
+        if not len(this_value):
+            print('Value not yet set')
+            return None
+        elif len(this_value) == 1:
+            print('value already set')
+            print(this_value[0].text)
+            return this_value[0]
+        else:
+            e=5/0
 
     def __setattr__(self, attr, value):
         print('Set attr', self.__dict__['_curpath'] + '/' + attr + '>>>' + str(value))
+        
+        curpath = self.__dict__['_curpath']
+        schema = self.__dict__['_schema']
+        path = curpath[1:] + '/' + attr
+        print('Get attr ', self.__dict__['_curpath'] + '/'+ attr)
+        print('Looking up in schema '+ curpath + '/' + attr)
+        this_schema = schema.xpath(curpath + '/' + attr)
+        if not len(this_schema):
+            raise BadVoodoo("Unable to find '%s' in the schema" % (curpath[1:] + '/' + attr))
+        elif len(this_schema) > 1:
+            raise BadVoodoo("Too many hits for '%s' in the schema" % (curpath[1:] + '/' + attr))
+
+
+        # TODO:
+        # validate against this_schema[0]
+        print('Schema to validate against...', this_schema[0])
+
+
+        xmldoc = self.__dict__['_xmldoc']
+        this_value = xmldoc.xpath(curpath + '/' + attr)
+        if len(this_value) == 0:
+            print('Value not yet set')
+            # This actually could be pretty tricky...
+            print('count of / = %s' % (path.count('/')))
+            if path.count('/') == 1:
+                new_node = etree.Element(attr)
+                new_node.text = str(value)
+                xmldoc.append(new_node)
+                return new_node
+                erint('Added brand new node because it happened to be at root and easy')
+        elif len(this_value) == 1:
+            this_value[0].attrib['old_value'] = this_value[0].text
+            this_value[0].text = str(value)
+            return this_value[0]
+            print('should be set here')
+        else:
+            c=5/0
 
     def create(self, *args):
         for arg in args:
