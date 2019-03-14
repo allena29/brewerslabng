e4182a218537b7f8edcea1920eb3557d6f240479
Adam Allen <allena29@users.noreply.github.com>
2019-03-14 20:44:23 +0000

    WIP: need a better looping method becuase xmldoc.iter() includes all the decedendts in lists

--- blng/Witch.py
+++ blng/Witch.py
@@ -20,13 +20,13 @@ class Witch:
         yield '--- CONFIGURATION START ---'
 
         last_path_count = 1
-        indent = ''
+        self.indent = ''
         for child in self.schema.iter():
             if 'cruxpath' in child.attrib:
                 cruxpath = child.attrib['cruxpath']
                 cruxtype = child.attrib['cruxtype']
                 path_count = cruxpath.count('/')-1
-                indent = '  ' * (path_count)
+                self.indent = '  ' * (path_count)
 
                 values = self.cfg.xpath('/voodoo'+cruxpath)
                 if not self.oldcfg:
@@ -37,29 +37,62 @@ class Witch:
                 if len(values) == 0 and len(old_values) == 0:
                     continue
 
+                raise ValueError(' need a better way to iterate here')
+                """
+                        old_session = DataAccess('crux-example.xml')
+                        session = DataAccess('crux-example.xml')
+
+                        old_root = old_session.get_root()
+                        old_root.simplelist.create('xyz')
+                        old_root.simplelist.create('123')
+                        old_root.simplelist.create('abc')
+
+                        root = session.get_root()
+                        root.simplelist.create('987')
+                        root.simplelist.create('xyz')
+                        root.simplelist.create('abc')
+                        root.simplelist.create('def')
+                        root.simplelist.create('ghi')
+                        root.simplelist.create('jkl')
+
+                    should not equal this
+                      --- CONFIGURATION START ---
+                        +
+                        - a
+                        - -   simplekey xyz;
+                        - +   simplekey 987;
+                          --- CONFIGURATION END ---
+
+                                        """
                 if cruxtype == 'list':
-                    raise ValueError('leists not supported')
+                    results = self._process_list_elements(values, old_values)
+                elif cruxtype == 'leaf':
+                    results = self._process_leaf_element(values, old_values)
+                else:
+                    results = self._process_nonleaf_element(values, old_values)
+                for result in results:
+                    yield result
 
-                if len(old_values) and len(values):
-                    # Old and New
-
-                    if cruxtype != 'leaf':
-                        yield '  ' + indent + old_values[0].tag + ':'
-                    else:
-                        yield '- ' + indent + old_values[0].tag + ' ' + old_values[0].text + ';'
-                        yield '+ ' + indent + old_values[0].tag + ' ' + values[0].text + ';'
-                elif len(values):
-                    # New
-                    if cruxtype != 'leaf':
-                        yield '+ ' + indent + values[0].tag + ':'
-                    else:
-                        yield '+ ' + indent + values[0].tag + ' ' + values[0].text + ';'
-                elif len(old_values):
-                    # Old
-                    if cruxtype != 'leaf':
-                        yield '- ' + indent + old_values[0].tag + ':'
-                    else:
-                        yield '- ' + indent + old_values[0].tag + ' ' + old_values[0].text + ':'
         b = time.time()
         self.log.debug('Processing took %s seconds', str(b-a))
         yield '--- CONFIGURATION END ---'
+
+    def _process_list_elements(self, cur, old):
+        return ['a']
+
+    def _process_nonleaf_element(self, cur, old):
+        if len(cur) and len(old):
+            return ['- ' + self.indent + old[0].tag + '::']
+        elif len(cur):
+            return ['+ ' + self.indent + cur[0].tag + '::']
+        elif len(old):
+            return ['- ' + self .indent + old[0].tag + '::']
+
+    def _process_leaf_element(self, cur, old):
+        if len(cur) and len(old):
+            return ['- ' + self.indent + old[0].tag + ' ' + old[0].text + ';',
+                    '+ ' + self.indent + cur[0].tag + ' ' + cur[0].text + ';']
+        elif len(cur):
+            return ['+ ' + self.indent + cur[0].tag + ' ' + cur[0].text + ';']
+        elif len(old):
+            return ['- ' + self .indent + old[0].tag + ' ' + old[0].text + ';']
--- test/unit/test_witch.py
+++ test/unit/test_witch.py
@@ -37,15 +37,44 @@ class TestVoodoo(unittest.TestCase):
 
         expected_result = """--- CONFIGURATION START ---
 + simpleleaf abc;
-+ bronze:
-+   silver:
-+     gold:
-+       platinum:
++ bronze::
++   silver::
++     gold::
++       platinum::
 +         deep down;
 - simpleenum 235;
 + simpleenum 234;
-- morecomplex:
--   leaf3 sfd:
+- morecomplex::
+-   leaf3 sfd;
+--- CONFIGURATION END ---
+"""
+        self.assertEqual(result, expected_result)
+
+    def test_simple_comparsion_with_lists(self):
+        old_session = DataAccess('crux-example.xml')
+        session = DataAccess('crux-example.xml')
+
+        old_root = old_session.get_root()
+        old_root.simplelist.create('xyz')
+        old_root.simplelist.create('123')
+        old_root.simplelist.create('abc')
+
+        root = session.get_root()
+        root.simplelist.create('987')
+        root.simplelist.create('xyz')
+        root.simplelist.create('abc')
+        root.simplelist.create('def')
+        root.simplelist.create('ghi')
+        root.simplelist.create('jkl')
+
+        w = Witch(root, old_root)
+
+        result = ''
+        for line in w.show():
+            result = result + line + '\n'
+
+        expected_result = """--- CONFIGURATION START ---
+
 --- CONFIGURATION END ---
 """
         self.assertEqual(result, expected_result)
