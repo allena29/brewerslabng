b8bd9061612fd5123b3301ecce436a896fed6a86
Adam Allen <allena29@users.noreply.github.com>
2019-04-28 18:59:05 +0100

    Imported ingredients into recipes.

--- blng/mysql-importer.py
+++ blng/mysql-importer.py
@@ -4,8 +4,9 @@ session.connect()
 root = session.get_root('brewerslab', yang_location='/sysrepo/yang')
 definition_root = session.get_root('brewerslab-definitions', yang_location='/sysrepo/yang')
 
+"""
 definition_root.definitions.ingredients.fermentables.create('sdf')
-#
+
 import mysql.connector
 con = mysql.connector.connect(host='192.168.1.13', database='brewerslab', user='brewerslab', password='beer')
 cursor = con.cursor()
@@ -43,5 +44,23 @@ for row in rows:
     yeast = definition_root.definitions.ingredients.yeast.create(row[0])
     yeast.attenuation = row[1]
 
+"""
+
+import mysql.connector
+query = "select recipeName,qty,ingredient,ingredientType from gIngredients WHERE ingredientType='fermentables'"
+con = mysql.connector.connect(host='192.168.1.13', database='brewerslab', user='brewerslab', password='beer')
+cursor = con.cursor()
+cursor.execute(query)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+    recipe = root.recipes.create(row[0], 'v1')
+
+    if row[2].count('Honey') or row[2].count('Syr'):
+        addat = "start of boil"
+    else:
+        addat = "mash"
+    ferm = recipe.ingredients.fermentables.create(row[2], addat)
+    ferm.qty = row[1]
 
 session.commit()
